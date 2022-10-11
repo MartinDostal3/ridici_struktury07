9b3efb4ddada2a75468d7ab8acf3212d6a07ed01
Dusty <martin.70cz@gmail.com>
2022-10-11 19:09:28 +0200

    switch

--- ridici_struktury07/Form1.Designer.cs
+++ ridici_struktury07/Form1.Designer.cs
@@ -31,19 +31,19 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(41, 30);
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(147, 28);
+            this.button1.Location = new System.Drawing.Point(139, 40);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -54,27 +54,29 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(41, 57);
+            this.listBox1.Location = new System.Drawing.Point(13, 85);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 368);
             this.listBox1.TabIndex = 2;
             // 
-            // button2
+            // comboBox1
             // 
-            this.button2.Location = new System.Drawing.Point(228, 27);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "obsahuje cislo?";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Foreach",
+            "For",
+            "While"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -90,7 +92,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- ridici_struktury07/Form1.cs
+++ ridici_struktury07/Form1.cs
@@ -22,35 +22,139 @@ namespace ridici_struktury07
             listBox1.Items.Clear();
             Random rnd = new Random();
             int n = int.Parse(textBox1.Text);
-            for(int i = 0; i < n; i++)
+            switch (comboBox1.SelectedItem.ToString())
             {
-                int nc = rnd.Next(-10, 51);
-                listBox1.Items.Add(nc);
+                case "Foreach":
+                    {
 
+                        for (int i = 0; i < n; i++)
+                        {
+                            int nc = rnd.Next(-10, 51);
+                            listBox1.Items.Add(nc);
+
+                        }
+
+                        bool obsahuje = false;
+
+                        foreach(int cislo in listBox1.Items)
+                        {
+                            if (cislo >= 20 && cislo <= 25)
+                            {
+                                obsahuje = true;
+                                break;
+                            }
+                        }
+                        if (obsahuje)
+                        {
+                            MessageBox.Show("Ano, posloupnost obsahuje cislo z intervalu (20;25>.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ne, posloupnost neobsahuje cislo z intervalu (20;25>.");
+                        }
+                    }
+                    break;
+                case "For":
+                    {
+
+                        
+                       
+                        for (int i = 0; i < n; i++)
+                        {
+                            int nc = rnd.Next(-10, 51);
+                            listBox1.Items.Add(nc);
+
+                        }
+
+                        bool obsahuje = false;
+                        for (int i = 0; i < listBox1.Items.Count && !obsahuje; i++)
+                        {
+                            int cislo = int.Parse(listBox1.Items[i].ToString());
+                            if (cislo >= 20 && cislo <= 25)
+                            {
+                                obsahuje = true;
+                            }
+                        }
+                        if (obsahuje)
+                        {
+                            MessageBox.Show("Ano, posloupnost obsahuje cislo z intervalu (20;25>.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ne, posloupnost neobsahuje cislo z intervalu (20;25>.");
+                        }
+
+
+                    }
+                    break;
+                case "While":
+                    {
+                        int i = 0;
+                        while(i<n)
+                        {
+                            int nc = rnd.Next(-10, 51);
+                            listBox1.Items.Add(nc);
+
+                            i++;
+                        }
+
+
+                        i = 0;
+                        bool obsahuje = false;
+                       while(i < listBox1.Items.Count && !obsahuje)
+                        {
+                            int cislo = int.Parse(listBox1.Items[i].ToString());
+                            if (cislo >= 20 && cislo <= 25)
+                            {
+                                obsahuje = true;
+                            }
+                            i++;
+                        }
+                        if (obsahuje)
+                        {
+                            MessageBox.Show("Ano, posloupnost obsahuje cislo z intervalu (20;25>.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ne, posloupnost neobsahuje cislo z intervalu (20;25>.");
+                        }
+                    }
+                    break;
+                default:
+                    {
+                        MessageBox.Show("Nevabrana moznost");
+                    }
+                    break;
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
 
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            bool obsahuje = false;
-            for(int i = 0; i < listBox1.Items.Count && !obsahuje; i++)
-            {
-                int cislo = int.Parse(listBox1.Items[i].ToString());
-                if(cislo >= 20 && cislo <= 25)
-                {
-                    obsahuje = true;
-                }
-            }
-            if(obsahuje)
-            {
-                MessageBox.Show("Ano, posloupnost obsahuje cislo z intervalu (20;25>.");
-            }
-            else
-            {
-                MessageBox.Show("Ne, posloupnost neobsahuje cislo z intervalu (20;25>.");
-            }
+           
         }
     }
 }
